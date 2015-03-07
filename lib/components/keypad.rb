2dc131465b02eb37b15022a7498a513ccd0c0d24
Michael Wayne <desperatethelark@gmail.com>
2015-03-06 21:48:58 -0800

    Fix bug where keypad was being treated like a hash on update

--- lib/components/keypad.rb
+++ lib/components/keypad.rb
@@ -3,7 +3,11 @@ class RubyChip::Keypad < RubyChip::Component
     RubyChip::Keypad.new Array.new(16, 0)
   end
 
-  def state= new_state
-    @state.merge! Hash[new_state]
+  def [] index
+    @state[index]
+  end
+
+  def []= index, value
+    @state[index] = value
   end
 end
