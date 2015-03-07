725de7a0155e5071f5d5c128e26ec45d234b4b21
Michael Wayne <desperatethelark@gmail.com>
2015-03-06 21:47:47 -0800

    Ensure program counter doesn't go out of range

--- lib/components/program_counter.rb
+++ lib/components/program_counter.rb
@@ -2,4 +2,8 @@ class RubyChip::ProgramCounter < RubyChip::Component
   def self.from_default
     RubyChip::ProgramCounter.new 0x200
   end
+
+  def state= new_state
+    @state = new_state % 0x1000
+  end
 end
