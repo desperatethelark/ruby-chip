e40233da6155810f5daf8f14ccd5cb8f63dd6863
Michael Wayne <desperatethelark@gmail.com>
2015-03-06 21:50:19 -0800

    Make sure register values stay in valid range

--- lib/components/register.rb
+++ lib/components/register.rb
@@ -4,10 +4,10 @@ class RubyChip::Register < RubyChip::Component
   end
 
   def self.default_state
-    (0..16).map { |n| [n, 0] }
+    (0...16).map { |n| [n, 0] }
   end
 
   def state= new_state
-    @state.merge! Hash[new_state]
+    @state.merge! Hash[new_state.map { |k,v| [k, v % 0x100] }]
   end
 end
