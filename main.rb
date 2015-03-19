924746d43b91924c7e06fbca2127fa1af2704156
Michael Wayne <desperatethelark@gmail.com>
2015-03-18 20:58:15 -0700

    add a check for the escape key for quicker exiting

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative './ruby_chip'
 class Window < Gosu::Window
   WIDTH  = RubyChip::WIDTH  * RubyChip::SCALE
   HEIGHT = RubyChip::HEIGHT * RubyChip::SCALE
-  FULLSCREEN = false
+  FULLSCREEN = true
   SPEED = 10
 
   def initialize
@@ -15,7 +15,11 @@ class Window < Gosu::Window
   end
 
   def update
-    8.times { update_user_input; update_vm_state }
+    SPEED.times do
+      update_user_input
+      update_vm_state
+    end
+
     check_for_sound_cue
     update_timers
   end
@@ -25,17 +29,13 @@ class Window < Gosu::Window
   end
 
   def draw
+    @vm[:draw_flag] = false
     scale(RubyChip::SCALE, RubyChip::SCALE) do
       RubyChip::PIXELS.each do |x, y|
         next if @vm.graphics.at_xy(x, y).zero?
-
-        draw_quad (x),      (y),      Gosu::Color::WHITE,
-                  (x),      (y + 1),  Gosu::Color::WHITE,
-                  (x + 1),  (y + 1),  Gosu::Color::WHITE,
-                  (x + 1),  (y),      Gosu::Color::WHITE
+        draw_pixel(x, y, x + 1, y + 1, Gosu::Color::WHITE)
       end
     end
-    @vm[:draw_flag] = false
   end
 
   # initialize helpers
@@ -55,6 +55,7 @@ class Window < Gosu::Window
   # update helpers
 
   def update_user_input
+    close if button_down?(Gosu::KbEscape)
     @key_map.map.each { |id, i| @vm[:keypad][i] = button_down?(id) ? 1 : 0 }
   end
 
@@ -68,6 +69,12 @@ class Window < Gosu::Window
 
   def update_timers
     [:delay_timer, :sound_timer].each { |t| @vm[t] = [@vm[t] - 1, 0].max }
+  end
+
+  # draw helpers
+
+  def draw_pixel(x1, y1, x2, y2, color)
+    draw_quad x1, y1, color, x1, y2, color, x2, y2, color, x2, y1, color
   end
 end
 
