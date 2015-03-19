2a1efcd588bee9f5c9d494bdc140a1312ba296ca
Michael Wayne <desperatethelark@gmail.com>
2015-03-18 20:54:01 -0700

    remove index changes that aren't part of modern interpreters

--- lib/opcodes/op_fx55.rb
+++ lib/opcodes/op_fx55.rb
@@ -2,7 +2,6 @@
 class OpFX55 < RubyChip::Instruction
   def changes_from_execution
     {
-      index:  i + x + 1,
       memory: (0..x).map { |v| [i + v, vm[:register][v]] }
     }
   end
--- lib/opcodes/op_fx65.rb
+++ lib/opcodes/op_fx65.rb
@@ -2,7 +2,6 @@
 class OpFX65 < RubyChip::Instruction
   def changes_from_execution
     {
-      index:    i + x + 1,
       register: (0..x).map { |v| [v, vm.memory.at(i + v)] }
     }
   end
